67113f81ead6dd97585c9d1daadc7a857adeca15
Andrey Polezhaiev <workpolezhaiev@gmail.com>
2025-03-20 00:05:49 +0200

    add mappings and show .env for test

--- src/test/java/com/polezhaiev/logistics/LogisticsApplicationTests.java
+++ src/test/java/com/polezhaiev/logistics/LogisticsApplicationTests.java
@@ -1,11 +1,24 @@
 package com.polezhaiev.logistics;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 class LogisticsApplicationTests {
+	@BeforeAll
+	static void loadEnv() {
+		Dotenv dotenv = Dotenv.configure()
+				.directory(System.getProperty("user.dir"))
+				.ignoreIfMissing()
+				.load();
 
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+	}
 	@Test
 	void contextLoads() {
 	}
